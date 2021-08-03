432103b2450536284f01dc81f822cd0c2abff3d8
Gebremeskel <gebre20122@gmail.com>
2021-08-02 20:28:05 -0500

    adding a routes for signup, login, google & delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'sessions#welcome'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :travels
   resources :trips
   resources :locations
