680145f29d9f8761181a2f4d66fa214bcbe68853
Gebremeskel <gebre20122@gmail.com>
2021-08-04 11:00:24 -0500

    adding a CRUD metheds

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,2 +1,50 @@
 class TravelsController < ApplicationController
+
+    def index
+        @travels = Travel.all
+    end
+
+    def show
+        @travel = Travel.find_by_id(params[:id])
+    end
+
+    def new
+        @travel = Travel.new
+    end
+
+    def create
+         # binding.pry
+         @travel = Travel.new(travel_params)
+         if @travel.save 
+           redirect_to travels_path
+         else
+           render :new
+         end
+    end
+
+    def edit
+        @travel = Travel.find_by_id(params[:id])
+    end
+
+    def update
+        @travel = Travel.find_by_id(params[:id])
+        @travel.update(travel_params)
+      if @travel.valid?
+        redirect_to travel_path(@travel)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+        @travel = Travel.find_by_id(params[:id])
+        @travel.destroy 
+        redirect_to travels_path
+    end
+
+    private
+    def travel_params
+        params.require(:travel).permit(:name, :address)
+    end
+        
 end
