6fc670056ad954325d5c8e51fb30066b9e2f4aee
Gebremeskel <gebre20122@gmail.com>
2021-08-10 07:22:19 -0500

    adding validation and find or create  attributes

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,29 @@ class Location < ApplicationRecord
   belongs_to :user
   has_many :trips
   has_many :travels, through: :trips
+  accepts_nested_attributes_for :trips,:travels, reject_if: proc { |attributes| attributes['city'].blank? || attributes['country'].blank?}
+ 
+  
+  validates :city, presence: true, length: {minimum: 2, message: "has to be longer than 2!!!"}
+  validates :country, presence: true
+  
+  def travels_attributes=(attributes)
+    attributes.values.each do |v|
+      self.travels << Travel.find_or_create_by(v) if !v['name']
+    end
+  end
+
+  def trips_attributes=(trip_hash)
+    if !trip_hash[:budget].blank? 
+      self.trip =Trip.find_or_create_by(trip_hash)
+    end
+  end
+  #alphabetize the list of locations
+  #scope :alpha, -> {'city'}
+  # scope :alpha, -> {order(:city)}
+
+
+  def city_and_country
+    "#{self.city} - #{self.country}"
+  end
 end
