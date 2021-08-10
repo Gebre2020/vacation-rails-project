724416e84047783b2115e06c6a85d3aa4df2ae9c
Gebremeskel <gebre20122@gmail.com>
2021-08-10 07:25:44 -0500

    adding validation, refactoring scope methods

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,4 +1,19 @@
 class Travel < ApplicationRecord
     has_many :trips
     has_many :locations, through: :trips
+
+    validates :name, presence: true
+    # validation comes from active record method to protct the database
+    validates :address, presence: true, uniqueness: true
+    #validate :too_many_travel
+
+    def name_and_address
+        "#{self.name} - #{self.address}"
+    end
+
+    def too_many_travel
+        if Travel.all.length >= 4
+            errors.add(:base, :invalid, message: "you have too many Travels!")
+        end
+    end
 end
