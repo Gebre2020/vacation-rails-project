ff0df82091a7db4871760bac86fb483ded504717
Gebremeskel <gebre20122@gmail.com>
2021-08-10 07:26:31 -0500

    adding validation and find or create  attributes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,21 @@
 class Trip < ApplicationRecord
   belongs_to :location
   belongs_to :travel
+  #accepts_nested_attributes_for :location, reject_if: proc { |attributes| attributes['city'].blank? || attributes['country'].blank?}
+  accepts_nested_attributes_for :travel, reject_if: proc { |attributes| attributes['name'].blank? || attributes['address'].blank?}
+  validates :budget, numericality: {greater_than: 0, less_than: 15000}
+
+  # def location_attributes=(location_hash)
+  #   #byebug
+  #   if !location_hash[:city].blank? && !location_hash[:country].blank?
+  #     self.location = Location.find_or_create_by(location_hash)
+  #   end
+  # end
+
+  def travel_attributes=(travel_hash)
+    if !travel_hash[:name].blank? && !travel_hash[:address].blank?
+      self.travel =Travel.find_or_create_by(travel_hash)
+    end
+  end
+
 end
