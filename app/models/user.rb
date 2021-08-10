18a8624f1026e365f3cc20fdd28dd252c6f6b13c
Gebremeskel <gebre20122@gmail.com>
2021-08-10 07:28:22 -0500

    adding validation and omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-    has_secure_password  # authenticate, password=, validate
     has_many :locations
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true
+    has_secure_password  # authenticate, password=, validate
+
+    def self.from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.email = auth['info']['email']
+            u.username = auth['info']['name']
+            u.password = SecureRandom.hex(15)
+        end
+    end
 end
