50380d4cb3326c247ef3825da5852f1bec04dfb5
Gebremeskel <gebre20122@gmail.com>
2021-08-11 23:56:46 -0500

    comment to travels & trips attributes

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,17 +8,20 @@ class Location < ApplicationRecord
   validates :city, presence: true, length: {minimum: 2, message: "has to be longer than 2!!!"}
   validates :country, presence: true
   
-  def travels_attributes=(attributes)
-    attributes.values.each do |v|
-      self.travels << Travel.find_or_create_by(v) if !v['name']
-    end
-  end
+  # def travels_attributes=(attributes)
+  #   attributes.values.each do |v|
+  #     self.travels << Travel.find_or_create_by(v) if !v['name']
+  #   end
+  # end
 
-  def trips_attributes=(trip_hash)
-    if !trip_hash[:budget].blank? 
-      self.trip =Trip.find_or_create_by(trip_hash)
-    end
-  end
+  # def trips_attributes=(attributes)
+  #   attributes.values.each do |trip_params|
+  #     if !trip_params.values.any?(&:empty?) && ((!trip_params["travel_attributes"]["name"].blank? && !trip_params["travel_attributes"]["address"].blank?)|| trip_params["travel_id"])
+  #       self.trips << Trip.create(trip_params)
+  #     end
+  #   end
+  # end
+  
   #alphabetize the list of locations
   #scope :alpha, -> {'city'}
   # scope :alpha, -> {order(:city)}
