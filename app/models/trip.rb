9394235f7de691674b36c8bf256b5be2ff99ae9b
Gebremeskel <gebre20122@gmail.com>
2021-08-12 10:37:39 -0500

    comment a travel attributes method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :location
   belongs_to :travel
-  #accepts_nested_attributes_for :location, reject_if: proc { |attributes| attributes['city'].blank? || attributes['country'].blank?}
+  accepts_nested_attributes_for :location, reject_if: proc { |attributes| attributes['city'].blank? || attributes['country'].blank?}
   accepts_nested_attributes_for :travel, reject_if: proc { |attributes| attributes['name'].blank? || attributes['address'].blank?}
   validates :budget, numericality: {greater_than: 0, less_than: 15000}
 
@@ -12,10 +12,10 @@ class Trip < ApplicationRecord
   #   end
   # end
 
-  def travel_attributes=(travel_hash)
-    if !travel_hash[:name].blank? && !travel_hash[:address].blank?
-      self.travel =Travel.find_or_create_by(travel_hash)
-    end
-  end
+  # def travel_attributes=(travel_hash)
+  #   if !travel_hash[:name].blank? && !travel_hash[:address].blank?
+  #     self.travel =Travel.find_or_create_by(travel_hash)
+  #   end
+  # end
 
 end
