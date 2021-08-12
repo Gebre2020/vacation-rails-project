2f7533aca4ff850f899d28d9f8158341b8891e00
Gebremeskel <gebre20122@gmail.com>
2021-08-11 23:52:33 -0500

    add codes into index, show, new, create action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,22 +2,33 @@ class LocationsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def index
-      @locations = Location.all
+      
+      if params[:user_id] && @user = User.find_by_id(params[:user_id])
+        @locations = @user.locations.all
+      else
+        flash[:message] = "The user doesn't exist" if params[:user_id]
+        @locations = Location.all
+      end
     end
 
     def show
       @location = Location.find_by_id(params[:id])
+      redirect_to locations_path if !@location
     end
 
     def new
-        @location = Location.new
+      @location = Location.new  
+      t = @location.trips.build
+      t.build_travel
+      # @location.build_user 
     end
 
     def create
-        @location = current_user.locations.build(location_params)
-        #binding.pry
+      @location = current_user.locations.build(location_params)
+      #@location = Location.new(location_params)
+      #@location.user_id = session[:user_id]
         if @location.save 
-          redirect_to locations_path
+          redirect_to location_path(@location)
         else
           render :new
         end
@@ -41,10 +52,10 @@ class LocationsController < ApplicationController
       @location = Location.find_by_id(params[:id])
       @location.destroy 
       redirect_to locations_path
-  end
+    end
 
-    private
+  private
     def location_params
-        params.require(:location).permit(:city, :country)
+        params.require(:location).permit(:city, :country, :user_id, travel_ids:[], user_attributes:[:username], trips_attributes:[:budget, :travel_id, travels_attributes:[:name, :address]])
     end
 end
