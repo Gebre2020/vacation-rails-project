ff6c6401e62b1856c61231323654dd96b29a3430
Gebremeskel <gebre20122@gmail.com>
2021-08-14 05:00:51 -0500

    adding accepts nested

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,10 +1,11 @@
 class Travel < ApplicationRecord
     has_many :trips
     has_many :locations, through: :trips
-
+    accepts_nested_attributes_for :locations
+    accepts_nested_attributes_for :trips
     validates :name, presence: true
     # validation comes from active record method to protct the database
-    validates :address, presence: true, uniqueness: true
+    validates :address, presence: true, uniqueness: {scope: :name}
     #validate :too_many_travel
 
     def name_and_address
