b0027077b59ba4d9fdd7ae0de214b3a9ae3365bd
Gebremeskel <gebre20122@gmail.com>
2021-08-14 04:56:54 -0500

    creating a helper methods for iteration html views

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,37 @@
-module TripsHelper
+module TripsHelper 
+    
+    def trip_price
+        if @trip.budget > 1500 
+            content_tag(:h6, "Trip Budget: #{number_to_currency(@trip.budget)}")
+           
+        else
+            content_tag(:h1, "Trip Budget: #{number_to_currency(@trip.budget)}") 
+            # <h1>Trip Budget Amount:-  number_to_currency(@trip.budget) </h1>
+        end 
+    end
+
+    def index_header_location
+         if @location
+            content_tag(:h1, "A Location: '#{@location.city} - #{@location.country}' Trip of Budgets:-")
+         else
+            content_tag(:h1, "All the Trips!!")
+         end
+    end
+
+    def form_header_location
+        if @location
+           content_tag(:h1, "Create: '#{@location.city} - #{@location.country}' Trip of Budgets:-")
+        else
+           content_tag(:h1, "Create Trip!")
+        end
+    end
+
+    def location_field_helper(f)
+        if @location 
+          f.hidden_field :location_id, value: @location.id 
+        else 
+          render partial: "build_location", locals: {f: f} 
+        end 
+    end
+
 end
