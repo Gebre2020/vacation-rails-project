d507d58a2ec0d60e134d2bc128c43706fe862d19
Gebremeskel <gebre20122@gmail.com>
2021-08-14 18:06:10 -0500

    adding record_not_found error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     # gives access to methods in the views
     # helper_method :current_user, :logged_in?
 
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    
     private
     # def current_user
     #     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
@@ -14,7 +16,14 @@ class ApplicationController < ActionController::Base
     #    # session[:user_id]
     # end
 
-    def redirect_if_not_logged_in
-        redirect_to '/' if !logged_in?
+    def redirect_if_not_logged_in?
+        redirect_to login_path if !logged_in?
+    end
+
+    def record_not_found(error)
+        #byebug
+        @message = error.message
+        # render :"layouts/error_page"
+        redirect_to :root, notice: "Page Not Found: #{@message}"
     end
 end
