2240686153094efbb8c4a6f73a4de818d1245aeb
Gebremeskel <gebre20122@gmail.com>
2021-08-15 06:59:55 -0500

    adding a scope & comment too_many_travel method

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -7,14 +7,21 @@ class Travel < ApplicationRecord
     # validation comes from active record method to protct the database
     validates :address, presence: true, uniqueness: {scope: :name}
     #validate :too_many_travel
+    scope :order_by_name, -> {order(:name)}
 
     def name_and_address
         "#{self.name} - #{self.address}"
     end
+    
+    # def too_many_travel
+    #     if Travel.all.length >= 4
+    #         errors.add(:base, :invalid, message: "you have too many Travels!")
+    #     end
+    # end
 
-    def too_many_travel
-        if Travel.all.length >= 4
-            errors.add(:base, :invalid, message: "you have too many Travels!")
+     def travel_attributes=(travel_hash)
+        if !travel_hash[:name].blank? && !travel_hash[:address].blank?
+        self.travel =Travel.find_or_create_by(travel_hash)
         end
     end
 end
