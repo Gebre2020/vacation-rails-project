f082c9cde4c722e634ac5dcd17f92110318a5c19
Gebremeskel <gebre20122@gmail.com>
2021-08-15 23:58:31 -0500

    change to find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
         end
     end
 
-    def show 
-        @user = User.find_by_id(params[:id])
+    def show       
+        @user = User.find(params[:id])
         redirect_to '/' if !@user
     end
 
